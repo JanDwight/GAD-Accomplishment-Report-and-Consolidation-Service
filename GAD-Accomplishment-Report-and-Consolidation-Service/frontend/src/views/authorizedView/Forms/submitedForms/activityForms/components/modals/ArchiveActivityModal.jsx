78015fab7b0a49d6a6c9c8be8da236a07060b664
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-04-23 14:34:52 +0800

    Added Loading, Success and Failed Feedbacks on Archive Function

--- GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/authorizedView/Forms/submitedForms/activityForms/components/modals/ArchiveActivityModal.jsx
+++ GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/authorizedView/Forms/submitedForms/activityForms/components/modals/ArchiveActivityModal.jsx
@@ -7,30 +7,29 @@ import Feedback from '../../../../../../components/feedbacks/Feedback';
 
 export default function ArchiveActivityModal({ selectedForm }) {
   // For feedback
-  const [error, setError] = useState('');
   const [message, setAxiosMessage] = useState('');
   const [status, setAxiosStatus] = useState('');
+
   const onSubmit = async (ev) => {
     ev.preventDefault();
-    setError({ __html: "" });
+
+    setAxiosMessage('Loading...');
+    setAxiosStatus('Loading');
 
     try {
       const response = await axiosClient.put(`/archive_form/${selectedForm.id}`, {});
-      setAxiosMessage(response.data.message); // Set success message
-      setAxiosStatus(response.data.Success);
-      setTimeout(() => {
-        setAxiosMessage(''); // Clear success message
-        setAxiosStatus('');
-      }, 3000); // Timeout after 3 seconds
+        setAxiosMessage(response.data.message);
+        setAxiosStatus(response.data.success); 
     } catch (error) {
-      setAxiosMessage(error.response.data.message); // Set success message
+      setAxiosMessage(error.message);
+      setAxiosStatus(false);
     }
   };
 
   return (
     <div>
       {/** For Feedback */}
-      <Feedback isOpen={message !== ''} onClose={() => setAxiosMessage('')} successMessage={message} status={status} />
+      <Feedback isOpen={message !== ''} onClose={() => setAxiosMessage('')} successMessage={message} status={status} refresh={false}/>
 
       <h1>
         Are you sure you want to delete <b>{selectedForm.title}</b>
