02820699e86961b5b6a4f6323a6abe3ec3b8db5b
rem <142568775+remismyname12@users.noreply.github.com>
2024-05-03 09:14:29 +0800

    confirmation_backup_and_restore

--- GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/components/backupAndRestore/Backup.jsx
+++ GAD-Accomplishment-Report-and-Consolidation-Service/frontend/src/views/components/backupAndRestore/Backup.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import NeutralButton from '../buttons/NeutralButton'
 import axiosClient from '../../axios/axios';
+import ReactModal from 'react-modal';
+import AddPrompt from '../../authorizedView/prompts/AddPrompt';
 
 export default function Backup() {
+
+  const [promptMessage, setPromptMessage] = useState('');
+  const [showPrompt, setShowPrompt] = useState(false);
+  const action = "Confirm Backup System?";
+
+   //<><><><><><>
+  const addprompt = (ev) => {
+    ev.preventDefault();
+    const concatmessage = "This action will create a backup of your system at it's current state. Do you wish to proceed?";
+    setPromptMessage(concatmessage);
+    setShowPrompt(true);
+  }
+
         const handleBackup = () => {
           // Make an HTTP POST request to trigger the backup process
           axiosClient.post('/backup/create')
@@ -18,7 +33,22 @@ export default function Backup() {
         
   return (
     <div>
-        <NeutralButton onClick={handleBackup}>Backup Database</NeutralButton>
+        <NeutralButton onClick={addprompt}>Backup Database</NeutralButton>
+        {/*----------*/}
+        <ReactModal
+              isOpen={showPrompt}
+              onRequestClose={() => setShowPrompt(false)}
+              className="md:w-[1%]"
+            >
+              <div>
+                  <AddPrompt
+                      closeModal={() => setShowPrompt(false)}
+                      handleSave={handleBackup}
+                      action={action}
+                      promptMessage={promptMessage}
+                  />
+              </div>
+        </ReactModal>
     </div>
   )
 }
